869a9ffaf45377d04ea5da0da06b29155f8d43f3
Kham Mangar <kham.mangar@outlook.com>
2022-03-20 00:22:22 -0400

    Update main.py
    comments

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+# import beautiful soup for webscraping and requests for grabing the website
 from bs4 import BeautifulSoup
 import requests
 
+# make a get request to the website
 response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 
+# extract the text
 movies_webpage = response.text
 
+# use beautifulsoup and html.parser/lxml
 soup = BeautifulSoup(movies_webpage, "html.parser")
 
-
+# use findAll to get the movie names
 movies = soup.findAll(name="h3", class_="title")
-# print(article_tag)
+
+# initialize an array to store the movies
 movie_title = []
 
+# use the for loop to store the movie in the list
 for movie in movies:
     movie_name = movie.get_text()
     movie_title.append(movie_name)
 
+# reverses the list because the websites list the movies in descending order
 movie_title.reverse()
 
+# creates a txt file containing the top 100 movies
 with open("movie_list.txt", "w") as file:
     for movie in movie_title:
         file.write(f"{movie}\n")
